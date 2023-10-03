9db52ce7a7fc67b3dab326db79fc7c2a45e1b1f4
zamirkhan <zamirkhan25@gmail.com>
2023-10-03 12:50:48 -0500

    Initial Commit

---
+++ app/src/main/java/UTD/cs/edu/pocket_poker/MainActivity.java
@@ -0,0 +1,14 @@
+package UTD.cs.edu.pocket_poker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
