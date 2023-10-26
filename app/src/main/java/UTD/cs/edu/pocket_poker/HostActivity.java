43e1cb6c2fd23284d40cd1c33c2d7eedec096401
Josh O <visualmoonlight1620@gmail.com>
2023-10-25 21:44:07 -0500

    added more classe and new .java file to handle intial portion of WifiDirectBroadcastReceiver

--- app/src/main/java/UTD/cs/edu/pocket_poker/HostActivity.java
+++ app/src/main/java/UTD/cs/edu/pocket_poker/HostActivity.java
@@ -1,7 +1,14 @@
 package UTD.cs.edu.pocket_poker;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -16,12 +23,51 @@ public class HostActivity extends AppCompatActivity {
     TextView read_msg_box, connectionStatus;
     EditText writeMsg;
 
+
+    WifiManager wifiManager;
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+
+    BroadcastReceiver mReceiver;
+    IntentFilter mIntentFiler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host);
         initialWork();
+        exqListener();
     }
+
+
+
+    /*
+    This works only somewhat, does not work on phone emulator but does work on my mobile device
+         I think because they changed/updated something after api 28 the .setWifiEnabled does not
+         work properly. You also have to manually toggle to wifi on and off for this reason
+         If wifi is on, then it will display "wifi is on"
+         otherwise it will display "wifi is off" \
+     */
+
+    /*
+    Technically this class is not really necessary but still wanted to try it out
+
+     */
+    private void exqListener() {
+        btnOnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (wifiManager.isWifiEnabled()) {
+                    wifiManager.setWifiEnabled(false);
+                    btnOnOff.setText("Wifi is ON");
+                } else {
+                    wifiManager.setWifiEnabled(true);
+                    btnOnOff.setText("Wifi is OFF");
+                }
+            }
+        });
+    }
+
 
     private void initialWork() {
         btnOnOff = (Button) findViewById(R.id.onOff);
@@ -31,8 +77,35 @@ public class HostActivity extends AppCompatActivity {
         read_msg_box = (TextView) findViewById(R.id.readMsg);
         connectionStatus = (TextView) findViewById(R.id.connectionStatus);
         writeMsg = (EditText) findViewById(R.id.writeMsg);
+
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+
+        mReceiver = new WifiDirectBroadcastReceiver(mManager, mChannel, this);
+
+        mIntentFiler = new IntentFilter();
+
+        mIntentFiler.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFiler.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFiler.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFiler.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFiler);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
 }
 
 
